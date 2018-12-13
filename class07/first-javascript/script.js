5974000359b1e1791a56de85fa6411bd2ecd8250
Ashley Chen <ashleychen414@gmail.com>
2018-12-12 21:28:56 -0500

    Updated RBG color-choice lab

---
+++ class07/first-javascript/script.js
@@ -0,0 +1,24 @@
+console.log ('test test');
+let sum = 10*10;
+let quotient = 100/10;
+let difference = 50-17;
+let addition = 50+17;
+let FirstName = 'Ashley';
+let LastName = 'Chen';
+
+console.log('sum');
+console.log('quotient');
+console.log('difference');
+console.log('addition');
+/*console.log(FirstName+' '+LastName);*/
+
+
+FirstName = 'Spiderman'
+
+let hello = document.querySelector('h1');
+
+console.log(hello);
+
+hello.style.color = 'blue';
+hello.innerHTML = 'Hello World! TEST TEST TEST ';
+
